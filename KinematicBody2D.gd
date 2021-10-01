736bc22bdaae734ce329bbb5bb93c80e4eec0917
Arnaud S <a.schmitt1859@gmail.com>
2021-10-01 15:46:45 +0200

    feature: Add simple code

---
+++ KinematicBody2D.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+const FLOOR_NORMAL = Vector2(0, -1)
+const SPEED = 200
+const GRAVITY = 10
+const JUMP_HEIGHT = 400
+var motion = Vector2()
+
+func _physics_process(delta):
+	motion.y += GRAVITY;
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x = SPEED
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+	else:
+		motion.x = 0
+		
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = -JUMP_HEIGHT
+
+	motion = move_and_slide(motion, FLOOR_NORMAL)
+	pass
