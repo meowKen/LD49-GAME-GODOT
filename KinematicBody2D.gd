c0e62c748871a9d1baa953fa9e9565b4e28e01bb
Arnaud S <a.schmitt1859@gmail.com>
2021-10-01 21:13:37 +0200

    feature: Add tileset and animation

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,18 +1,32 @@
 extends KinematicBody2D
 
 const FLOOR_NORMAL = Vector2(0, -1)
-const SPEED = 200
-const GRAVITY = 10
-const JUMP_HEIGHT = 300
+const SPEED = 300
+const GRAVITY = 40
+const JUMP_HEIGHT = 700
 var motion = Vector2()
+onready var _animation_player = $Animation
+	
+func _process(delta):
+	if motion.y < -10:
+		_animation_player.play("jump_up")
+	elif motion.y > 10:
+		_animation_player.play("jump_down")
+	elif motion.x != 0:
+		_animation_player.play("run")
+	elif motion.x == 0:
+		_animation_player.play("idle")
+	pass
 
 func _physics_process(delta):
-	motion.y += GRAVITY;
+	motion.y = motion.y + GRAVITY if motion.y < 1000 else motion.y
 	
 	if Input.is_action_pressed("ui_right"):
 		motion.x = SPEED
+		_animation_player.flip_h = false
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -SPEED
+		_animation_player.flip_h = true
 	else:
 		motion.x = 0
 		
